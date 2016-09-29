70fd88fe2efa2b197800f862c1d330fe54317362
kmoua92 <kermoua92@gmail.com>
2016-09-28 19:59:20 -0700

    Implement SongModel

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,24 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    // listen to changes in the queue
+    // debugger;
+    // this.on('change', function(event) {
+    //   if (this.models.length === 0) {
+    //     this.models[0].playFirst();
+    //   } else {
+
+    //   }
+    // }, this);
+  },
+
+  add: function(song) {
+    var s = new SongModel(song);
+    this.models.push(s);
+  },
+
+  playFirst: function() {
+    console.log('inside play first');
   }
 
 });
--- test/spec/SongQueueSpec.js
+++ test/spec/SongQueueSpec.js
@@ -20,8 +20,9 @@ describe('SongQueue', function() {
   });
 
   describe('when a song is added', function() {
-    xit('plays it if it is the only song in the song queue', function() {
+    it('plays it if it is the only song in the song queue', function() {
       var songQueue = new SongQueue();
+      // debugger;
       songQueue.add(songData1);
       expect(playSpy).to.have.been.called;
     });
