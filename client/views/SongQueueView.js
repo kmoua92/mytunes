4a86742505ca6bb859ce556a8a0dc022dab6b673
kmoua92 <kermoua92@gmail.com>
2016-09-29 12:24:38 -0700

    Add song queue and song queue view

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,17 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
   },
 
   render: function() {
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
     return this.$el;
-  }
-
+  },
 });
