f16689af064336d117dcf99f6aabb8afc488b263
kmoua92 <kermoua92@gmail.com>
2016-09-29 20:48:45 -0700

    Implement fetch update from PARSE

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,23 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function() {
+    this.fetch();
+  },
+
+  fetch: function() {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs/',
+      type: 'GET',
+      success: function(data) {
+        this.reset(data.results);
+      }.bind(this),
+      error: function() {
+        console.log('failed to fetch data');
+      }
+    });
+  }
 
 });
